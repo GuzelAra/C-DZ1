911957171ff96e1cce23448e5f553af20d3d1e43
Guzel Elanskaya <guzelarazova@yandex.ru>
2022-11-14 21:58:04 +0300

    added Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное
+// из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// // 22 3 9 -> 22
+
+Console.WriteLine("Введите число А");
+int numberA=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numberB=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число C");
+int numberC=Convert.ToInt32(Console.ReadLine());
+if(numberA>numberB)
+    {
+        if(numberA>numberC){Console.WriteLine("А максимальное");}  
+        else 
+        {Console.WriteLine("C максимальное");} 
+    }
+else 
+    {
+        if(numberB>numberC){Console.WriteLine("B максимальное");}
+    else 
+        {Console.WriteLine("C максимальное");}
+    }
+
+
